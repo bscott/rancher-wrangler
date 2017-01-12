718706229a423062331969befd2fc8a41a9fd1a2
Brian Scott <brainscott@gmail.com>
2017-01-11 23:19:22 -0800

    Created Host model and added Struct fields

--- actions/home.go
+++ actions/home.go
@@ -7,3 +7,5 @@ import "github.com/gobuffalo/buffalo"
 func HomeHandler(c buffalo.Context) error {
 	return c.Render(200, r.HTML("index.html"))
 }
+
+
---
+++ models/host.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"time"
+
+	"encoding/json"
+
+	"github.com/markbates/pop"
+
+	"github.com/markbates/validate"
+)
+
+type Host struct {
+	ID int `json:"id" db:"id"`
+	Url string `json:"url" db:"url"`
+	ApiVersion string `json:"api_version" db:"api_version"`
+	AccessKey string `json:"access_key" db:"access_key"`
+	SecretKey string `json:"secret_key" db:"secret_key"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (h Host) String() string {
+	b, _ := json.Marshal(h)
+	return string(b)
+}
+
+// Hosts is not required by pop and may be deleted
+type Hosts []Host
+
+// String is not required by pop and may be deleted
+func (h Hosts) String() string {
+	b, _ := json.Marshal(h)
+	return string(b)
+}
+
+// Validate gets run everytime you call a "pop.Validate" method.
+// This method is not required and may be deleted.
+func (h *Host) Validate(tx *pop.Connection) (*validate.Errors, error) {
+
+	return validate.NewErrors(), nil
+
+}
+
+// ValidateSave gets run everytime you call "pop.ValidateSave" method.
+// This method is not required and may be deleted.
+func (h *Host) ValidateSave(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run everytime you call "pop.ValidateUpdate" method.
+// This method is not required and may be deleted.
+func (h *Host) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
