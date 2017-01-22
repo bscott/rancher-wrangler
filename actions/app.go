049d8f97bf04a08338f0eb3c52e6f5fa58d623fb
Brian Scott <brian@bscott.io>
2017-01-21 16:10:58 -0800

    Updated edit form but getting an crash - some value is nil

--- actions/app.go
+++ actions/app.go
@@ -31,7 +31,7 @@ func App() *buffalo.App {
 		app.GET("/hosts/create", HostsCreate)
 		app.GET("/hosts/show", HostsShow)
 		app.POST("/hosts/new", HostsNew)
-		app.PUT("/hosts/update", HostsUpdate)
+		app.GET("/hosts/{id}/edit", HostsEdit)
 		app.ServeFiles("/assets", assetsPath())
 	}
 
